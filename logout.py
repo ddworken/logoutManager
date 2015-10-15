2da5ecd5239fcf3bcb8bd1a391f4b64407367403
David Dworken <david@daviddworken.com>
2015-10-15 17:21:27 -0400

    Create logout.py

---
+++ logout.py
@@ -0,0 +1,24 @@
+#!/bin/python
+from os.path import isfile
+from os import system
+
+sleepCommand = "gnome-screensaver-command -l"
+
+if isfile("/proc/acpi/button/lid/LID/state"):
+    filename = "/proc/acpi/button/lid/LID/state"
+else:
+    if isfile("/proc/acpi/button/lid/LID0/state"):
+        filename = "/proc/acpi/button/lid/LID/state"
+    else:
+        print "Could not find the lid state. "
+        exit()
+
+while True:
+    with open(filename) as f:
+        state = f.read().splitlines()[0]
+    if "open" in state:
+        lastLoopWasClosed = False
+    else:
+        if not lastLoopWasClosed:
+            system(sleepCommand)
+            lastLoopWasClosed = True
